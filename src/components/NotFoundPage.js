5422bbc0e7b0946a6ba9596a5a27c369ca4af352
Calvin Jin <jinchi2013us@gmail.com>
2016-10-30 20:29:02 -0700

    add not found page

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class NotFoundPage extends React.Component {
+    render() {
+        return(
+            <div className="not-found">
+                <h1>404</h1>
+                <h2>Page not found !!!</h2>
+                <p>
+                    <Link to="/">Go Back to the main page!</Link>
+                </p>
+            </div>
+        );
+    }
+}
